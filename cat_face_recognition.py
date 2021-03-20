9b509fe5d69479b62805eb051b65db7e6b704853
Katie Huang <75452581+katiehuang1221@users.noreply.github.com>
2021-03-20 00:25:59 -0700

    Update cat_face_recognition.py

--- cat_face_recognition.py
+++ cat_face_recognition.py
@@ -3,23 +3,13 @@ import cv2 as cv
 
 haar_cascade = cv.CascadeClassifier('haar_catface.xml')
 
-people = ['Noodle', 'Chiffon','Cheddar']
-# features = np.load('features.npy',allow_pickle=True)
-# labels = np.load('labels.npy')
+cats = ['Noodle', 'Chiffon','Cheddar']
 
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('cat_face_trained.yml')
 
-
-# Chiffon
-# img = cv.imread(r'/Users/katiehuang/Documents/CV/Cat_faces/Test/IMG_0122.jpeg')
-# Cheddar
-# img = cv.imread(r'/Users/katiehuang/Documents/CV/Cat_faces/Test/IMG_0123.jpeg')
-# Noodle
-# img = cv.imread(r'/Users/katiehuang/Documents/CV/Cat_faces/Test/IMG_0124.jpg')
-# Chiffon+Cheddar
+# Image for testing
 img = cv.imread(r'/Users/katiehuang/Documents/OpenCV/Cat_faces/Test/IMG_0135.jpg')
-# img = cv.imread(r'/Users/katiehuang/Documents/CV/Cat_faces/Test/IMG_0091.jpeg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Person',gray)
@@ -37,10 +27,5 @@ for (x,y,w,h) in faces_rect:
     cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), thickness=2)
     
 cv.imshow('Detected Cat Face', img)
-
-
-
-
-
 cv.waitKey(0)
 
